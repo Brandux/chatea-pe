fac8de26c659432cc8e83260f6030463f98ee6b4
Brandux <branduxjuarez@upeu.edu.pe>
2018-12-02 00:04:26 -0500

    Prueba git con visual studio code

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,18 @@ import { LoginComponent } from './login/login.component';
 import { HomeComponent } from './home/home.component';
 import { ConversationComponent } from './conversation/conversation.component';
 import { ProfileComponent } from './profile/profile.component';
+import { Routes, RouterModule } from '@angular/router';
+import { MenuComponent } from './menu/menu.component';
+
+// Esta constante esta referenciando a los modulos que seran accedidos mediante la URL
+// http://localhost:4000/home/
+const appRoute: Routes = [
+  { path: '' , component: HomeComponent},
+  { path: 'home' , component: HomeComponent },
+  { path: 'login', component: LoginComponent},
+  { path: 'profile', component: ProfileComponent},
+  { path: 'conversation', component: ConversationComponent}
+];
 
 @NgModule({
   declarations: [
@@ -14,11 +26,13 @@ import { ProfileComponent } from './profile/profile.component';
     LoginComponent,
     HomeComponent,
     ConversationComponent,
-    ProfileComponent
+    ProfileComponent,
+    MenuComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot(appRoute) /* <- importamos la ruta de los modulos  */
   ],
   providers: [],
   bootstrap: [AppComponent]
